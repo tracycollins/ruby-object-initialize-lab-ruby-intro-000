da29f227c9e4d8a507f9e2beacc77041403cebd6
Tracy Collins <tc@threeceemedia.com>
2020-02-23 00:47:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,19 +4,19 @@ class Dog
     @breed = breed
   end
 
-    def name=(name)
-      @name = name
-    end
+  def name=(name)
+    @name = name
+  end
 
-    def name
-      name
-    end
+  def name
+    name
+  end
 
-      def breed=(breed)
-        @breed = breed
-      end
+  def breed=(breed)
+    @breed = breed
+  end
 
-      def breed
-        breed
-      end
+  def breed
+    breed
+  end
 end
